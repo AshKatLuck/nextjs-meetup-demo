f48c37846ec9e243b5ed836104cb6e8852e09268
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-03-15 17:11:05 -0700

    revalidate the home page evry 10s

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,14 @@ const DUMMY_meetups = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_meetups} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+export async function getStaticProps() {
+  return {
+    props: { meetups: DUMMY_meetups },
+    revalidate: 10,
+  };
 }
 
 export default HomePage;
